fdeadfb65d0caa8a3fa69c39bd9badf2b28611c3
LiangJavaDS <954157304@qq.com>
2022-05-11 12:36:51 +0800

    前后端建立http链接

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,23 @@
 import styles from './index.less';
 import React from 'react';
+import axios from 'axios';
 
 export default function IndexPage() {
+  const get = () => {
+    // 上面的请求也可以这样做
+    axios.get('http://127.0.0.1:8080/')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <button onClick={get}>点击获取</button>
     </div>
   );
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,46 @@
-use ferris_says::say;
-use std::io::{stdout,BufWriter};
+use actix_cors::Cors;
+use actix_web::http::header;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-fn main() {
-    // println!("Hello, world!");
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
 
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
 
-    let mut writer = BufWriter::new (stdout.lock());
-    say(message.as_bytes(),width,&mut writer).unwrap();
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hey there!")
+// }
+
+#[get("/")]
+async fn get_text(txt: String) -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(format!("成功接收到了:{}", txt))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin("http://localhost:8000")
+                    .allowed_methods(vec!["GET", "POST", "DELETE"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .max_age(3600), // .supports_credentials(), // Allow the cookie auth.
+            )
+            .service(get_text)
+        // .service(hello)
+        // .service(echo)
+        // .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
